65ace2f7e3f01928835500265f12fb3cdf7e9d86
14438 <180376435+Hor1zen@users.noreply.github.com>
2025-12-27 21:12:29 +0800

    编辑功能

--- blogs/urls.py
+++ blogs/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     # 主页
     path('', views.index, name='index'),
     path('new_post/', views.new_post, name='new_post'),
+    # <int:post_id> 是用来接收文章ID的占位符
+    path('edit_post/<int:post_id>/', views.edit_post, name='edit_post'),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,5 @@
 # blogs/views.py
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import BlogPost
 from .forms import BlogPostForm
 
@@ -25,3 +25,21 @@ def new_post(request):
     # 显示空表单或无效表单
     context = {'form': form}
     return render(request, 'blogs/new_post.html', context)
+
+def edit_post(request, post_id):
+    """编辑既有帖子"""
+    # 根据 ID 获取帖子，如果找不到这就返回 404
+    post = get_object_or_404(BlogPost, id=post_id)
+
+    if request.method != 'POST':
+        # 初次请求：使用当前帖子的内容填充表单
+        form = BlogPostForm(instance=post)
+    else:
+        # POST 提交的数据：对数据进行处理
+        form = BlogPostForm(instance=post, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('blogs:index')
+
+    context = {'post': post, 'form': form}
+    return render(request, 'blogs/edit_post.html', context)

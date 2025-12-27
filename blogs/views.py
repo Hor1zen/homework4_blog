71946f4b80cfada0177600f3251e852316ac5e28
14438 <180376435+Hor1zen@users.noreply.github.com>
2025-12-27 20:41:21 +0800

    视图函数

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,15 @@
+# blogs/views.py
 from django.shortcuts import render
+from .models import BlogPost
 
-# Create your views here.
+
+def index(request):
+    """主页：显示所有帖子"""
+    # 1. 查询数据库：按 date_added 降序排列 (最新在前)
+    posts = BlogPost.objects.order_by('-date_added')
+
+    # 2. 把数据打包成字典
+    context = {'posts': posts}
+
+    # 3. 渲染网页
+    return render(request, 'blogs/index.html', context)
